6543fa6a563e89824874daff7333fdeb07e3dff9
aapolo7 <orlandoconceicao94@gmail.com>
2026-02-06 18:54:23 -0400

    fix: dependencias seguras

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+SECRET_KEY = os.getenv("SECRET_KEY", 60)
+ALGORITHM = os.getenv("ALGORITHM", 60)
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 app = FastAPI()
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
